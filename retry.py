efc7ea1d5c5279cc73bbc987ae82b8638389f389
Mikhail <zegmund84@gmail.com>
2026-08-08 18:24:14 +0700

    chore: parametrize TELEGRAM_ADMIN_ID in retry.py and update .gitignore

--- retry.py
+++ retry.py
@@ -1,3 +1,4 @@
+import os
 import asyncio
 
 from arq import create_pool
@@ -5,8 +6,9 @@ from arq.connections import RedisSettings
 
 
 async def main():
+    chat_id = int(os.getenv("TELEGRAM_ADMIN_ID", "0"))
     redis = await create_pool(RedisSettings(host='localhost', port=6379))
-    await redis.enqueue_job('process_video', 'b19a37a4-4ae0-4f2b-a776-0986fb908b03', 'https://www.instagram.com/reel/DbTfh6PxJUr/?igsh=NHV2NnU2NTFjanRh', 392046103)
+    await redis.enqueue_job('process_video', 'b19a37a4-4ae0-4f2b-a776-0986fb908b03', 'https://www.instagram.com/reel/DbTfh6PxJUr/?igsh=NHV2NnU2NTFjanRh', chat_id)
     print("Job enqueued!")
 
 asyncio.run(main())
